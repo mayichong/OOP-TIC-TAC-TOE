57546fb569a96a49420dd9543f614accade78290
Yichong Ma <kevin@Yichongs-MacBook-Pro.local>
2019-03-26 02:18:11 -0400

    Alpha

--- src/oop/application/Main.java
+++ src/oop/application/Main.java
@@ -1,18 +1,20 @@
-package application;
+package oop.application;
 	
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-
+import oop.view.View;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			View mView = new View();
+			Scene scene = mView.getMainScene();
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+
+			primaryStage.setTitle("Tic Tac Toe");
+
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
---
+++ src/oop/others/Players.java
@@ -0,0 +1,14 @@
+package oop.others;
+
+public class Players {
+    private String username;
+    private String marker;
+
+    public Players (String username,String marker){
+        this.username = username;
+        this.marker = marker;
+    }
+
+
+
+}
